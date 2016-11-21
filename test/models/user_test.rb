4300a27d0cec9ae6ba919c261fcb925dff063940
Peng Liu <pengliu12@gmail.com>
2016-11-21 02:49:59 -0500

    added base rails app (#4)
    * added base rails app
    
    * switched to MySQL

---
+++ test/models/user_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+    test 'should not save user without name or email' do
+        user = User.new
+        user.name = 'Test'
+        assert_not user.save, "Saved #{user} without a name or email"
+        user = User.new
+        user.email = 'Test'
+        assert_not user.save, "Saved #{user} without a name or email"
+    end
+end
