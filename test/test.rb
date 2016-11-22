5e7320c8716329d29d2e9bfccb12f1d985727a78
Peng Liu <pengliu12@gmail.com>
2016-11-21 22:32:02 -0500

    fixed test exit code always being 0

--- test/test.rb
+++ test/test.rb
@@ -1,10 +1,7 @@
 require 'test/unit'
-require 'test/unit/ui/console/testrunner'
 
 class TC_MyTest < Test::Unit::TestCase
-  def test_fail
-    assert(false, 'Assertion was false.')
-  end
+    def test_pass
+      assert(false, 'Assertion was false.')
+    end
 end
-
-Test::Unit::UI::Console::TestRunner.run(TC_MyTest)
