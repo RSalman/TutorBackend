f03ab3b8fd54a99459f6a4f87900877e63ba6a1d
Sarmad Hashmi <sarmadhashmi@gmail.com>
2016-12-14 00:19:21 -0500

    Add initial API V1 stuff (#20)

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      respond_to :json
+
+      def index
+        respond_with User.all
+      end
+
+      def show
+        respond_with User.find(params[:id])
+      end
+
+      def create
+        respond_with User.create(params[:user])
+      end
+
+      def update
+        respond_with User.update(params[:id], params[:user])
+      end
+
+      def destroy
+        respond_with User.destroy(params[:id])
+      end
+    end
+  end
+end
