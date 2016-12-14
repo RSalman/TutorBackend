a58a9c9d71c83a28cfaf09e30fc081b69628f470
Sarmad Hashmi <sarmadhashmi@gmail.com>
2016-12-14 00:19:21 -0500

    Add initial API V1 stuff (#20)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
   include DeviseTokenAuth::Concerns::SetUserByToken
     protect_from_forgery with: :exception
 end
