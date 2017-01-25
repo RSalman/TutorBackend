7c9cc236052bb0141b658a9873237086440c7667
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-01-25 15:28:18 -0500

    Add phone verification using Authy (#32)
    * Add phone verification, i.e: send code via sms to user, user confirms code
    
    * Add Authy sandbox credentials and remove bcrypt mingw32

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :destroy, :update]
+      post :start_verify_phone, to: 'phone_verification#start'
+      post :check_verify_phone, to: 'phone_verification#check'
     end
   end
 end
