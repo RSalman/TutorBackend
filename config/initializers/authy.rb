6656b75cc10e53482a7bc2304fd73116fe3ddeea
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-01-25 00:40:10 -0500

    Add Authy sandbox credentials and remove bcrypt mingw32

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,4 +1,4 @@
 require 'authy'
 
-Authy.api_key = 'your-api-key'
-Authy.api_uri = 'https://api.authy.com/'
+Authy.api_key = ENV['AUTHY_API_KEY'] || "bf12974d70818a08199d17d5e2bae630"
+Authy.api_uri = ENV['AUTHY_API_URL'] || "http://sandbox-api.authy.com"
