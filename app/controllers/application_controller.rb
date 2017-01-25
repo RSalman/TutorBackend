d06cf6974ae82a3783e3f845d9ab9d4ce24a9587
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-01-24 22:48:51 -0500

    Remove CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 # Base controller
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
 end
