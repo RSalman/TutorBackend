6d17461177ee5255e74220b54599e8b35e4fdfb7
Salman Rana <srana026@uottawa.ca>
2017-03-13 22:47:00 -0400

    notification controller -  removed debuging code

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -9,7 +9,6 @@ module Api
         user = User.find(params[:id])
         token_data = params[:token_data]
         updated = user.update(app_token: token_data[:app_token], app_token_platform: token_data[:app_token_platform])
-        send_notification(1, 'test', 'test')
         # TODO: Add error-handling
         json_response('updated' => updated)
       end
