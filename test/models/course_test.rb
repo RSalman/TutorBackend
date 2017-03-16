66bddc0b796fb8883bbc6b25a47bcc779edbbd18
Peng Liu <pengliu12@gmail.com>
2017-03-16 01:42:19 -0400

    Test
    Added model test for new constraints on Courses

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -23,4 +23,13 @@ class CourseTest < ActiveSupport::TestCase
     refute course.valid?, 'course is valid without name'
     assert_not_empty course.errors[:course_name], 'no validation error for course_name present'
   end
+
+  test 'invalid length too long' do
+    course = Course.create(course_prefix: '12345678901', course_code: '2132')
+    refute course.valid?, 'course is valid with prefix too long'
+    assert_not_empty course.errors[:course_prefix], 'no validation error for course_prefix too long'
+    course = Course.create(course_prefix: 'CSI', course_code: '12345678901')
+    refute course.valid?, 'course is valid with code too long'
+    assert_not_empty course.errors[:course_code], 'no validation error for course_code too long'
+  end
 end
