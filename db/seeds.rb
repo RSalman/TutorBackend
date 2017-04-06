08c8c2316dda560d28a7e5351ea110684b249801
Salman Rana <srana026@uottawa.ca>
2017-04-06 19:42:09 -0400

    seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,15 @@
 sample_bio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. sheets containing Lorem Ipsum passages, and more recently with."
 sample_caption = "Passionate about sharing knowledge!"
 
-tutor = User.create!({'first_name': 'Tutor', 'last_name': 'Test', 'password': 'password',
-                       'email': 'tutor@test.com', 'uid': '12345', 'phone_number': '6131234567', 'tutor_description': sample_bio, 'tutor_short_description': sample_caption})
+tutor = User.create!({'first_name': 'Salman', 'last_name': 'Rana', 'password': 'password',
+                       'email': 'srana@uottawa.ca', 'uid': '12345', 'phone_number': '6131234567', 'tutor_description': sample_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 25, 'num_tutor_rating': 10})
 
-test_course = Course.create!({'course_prefix': 'TST', 'course_code': '0001', 'course_name': 'Notifications Course'})
+test_course = Course.create!({'course_prefix': 'SEG', 'course_code': '4145', 'course_name': 'Real Time Systems'})
 tutor_subject = TutorSubject.create!({'user_id': tutor.id, 'course_id': test_course.id, 'rate': 15})
 
-tutee = User.create!({'first_name': 'Tutee', 'last_name': 'Test', 'password': 'password',
-                       'email': 'tutee@test.com', 'uid': '12346', 'phone_number': '6131234568'})
+tutee = User.create!({'first_name': 'Sarmad', 'last_name': 'Test', 'password': 'password',
+                       'email': 'shashmi@uottawa.ca', 'uid': '12346', 'phone_number': '6131234568', 'tutor_hidden': 0})
 
 encrypted_password = User.new(password: 'password').encrypted_password
 User.bulk_insert(:first_name, :last_name, :phone_number, :encrypted_password, :email, :uid,
