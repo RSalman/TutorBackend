2db9b199d29ae5a83d2254492e9d3d5cb5e824ed
Salman Rana <srana026@uottawa.ca>
2017-10-05 01:58:19 -0400

    Removed unnecessary delete all

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -40,19 +40,38 @@ module Api
         user = User.find(params[:id])
         user.update(user_params)
 
+        old_courses_obj = Course.joins(tutor_subjects: :user)
+                .select('courses.course_prefix, courses.course_code, tutor_subjects.id')
+                .where('tutor_subjects.user_id = ' + params[:id])
+
+        old_courses = []
+        old_courses_obj.each do |course|
+          old_courses << course.course_prefix + course.course_code
+        end
+
         if user.valid?
-          courses = params[:courseList]
-          if courses
-            # Delete all the users courses
-            TutorSubject.where(:user_id => params[:id]).destroy_all
-            courses.each do |course|
+          updated_courses = params[:courseList]
+          if updated_courses
+            courses_to_remove = old_courses - updated_courses
+            courses_to_add = updated_courses - old_courses
+
+            courses_to_remove.each do |course|
               /(?<prefix>[[:alpha:]]*)[[:space:]]*(?<code>[[:digit:]]*)/ =~ course
               next unless prefix and code
-              course = Course.where(course_code: code, course_prefix: prefix).first
-              if course.nil?
-                course = Course.create(course_prefix: prefix, course_code: code, course_name: 'N/A')
+              course_to_remove = Course.where(course_code: code, course_prefix: prefix).first
+              if course_to_remove
+                TutorSubject.where(user_id: user.id, course_id: course_to_remove.id).first.destroy
               end
-              TutorSubject.create(user_id: user.id, course_id: course.id, rate: params[:rate])
+            end
+
+            courses_to_add.each do |course|
+              /(?<prefix>[[:alpha:]]*)[[:space:]]*(?<code>[[:digit:]]*)/ =~ course
+              next unless prefix and code
+              course_to_add = Course.where(course_code: code, course_prefix: prefix).first
+              if course_to_add.nil?
+                course_to_add = Course.create(course_prefix: prefix, course_code: code, course_name: 'N/A')
+              end
+              TutorSubject.create(user_id: user.id, course_id: course_to_add.id, rate: params[:rate])
             end
           end
           json_response(user, :created)
