cd9bd08d0e7083b28ab23b328e9b2fbc2aeee9c1
Muraad Hared <mhare095@uottawa.ca>
2017-10-12 22:52:46 -0400

    Updated default db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,75 +10,48 @@
 education = ["Masters", "PHD", "Bachelors", "High School Diploma"]
 
 sample_bio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. sheets containing Lorem Ipsum passages, and more recently with."
+sample_short_bio = "I am a fourth year Software Engineering Student with a 9.9GPA"
 sample_caption = "Passionate about sharing knowledge!"
 
-tutor = User.create!({'first_name': 'Salman', 'last_name': 'Rana', 'password': 'password',
-                       'email': 'srana@uottawa.ca', 'uid': '12345', 'phone_number': '6131234567', 'tutor_description': sample_bio, 'tutor_short_description': sample_caption,
-                       'agg_tutor_rating': 25, 'num_tutor_rating': 10, 'education': 'Masters'})
+tutor_salman = User.create!({'first_name': 'Salman', 'last_name': 'Rana', 'password': 'password',
+                       'email': 'srana@uottawa.ca', 'phone_number': '6131234567', 'tutor_description': sample_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 25, 'num_tutor_rating': 10, 'education': education[0]})
 
-test_course = Course.create!({'course_prefix': 'SEG', 'course_code': '4145', 'course_name': 'Real Time Systems'})
-tutor_subject = TutorSubject.create!({'user_id': tutor.id, 'course_id': test_course.id, 'rate': 15})
+tutor_sarmad = User.create!({'first_name': 'Sarmad', 'last_name': 'Hashmi', 'password': 'password',
+                       'email': 'shashmi@uottawa.ca', 'phone_number': '6137410000', 'tutor_description': sample_short_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 40, 'num_tutor_rating': 10, 'education': education[1]})
 
-tutee = User.create!({'first_name': 'Sarmad', 'last_name': 'Test', 'password': 'password',
-                       'email': 'shashmi@uottawa.ca', 'uid': '12346', 'phone_number': '6131234568', 'tutor_hidden': 0})
+tutor_muraad = User.create!({'first_name': 'Muraad', 'last_name': 'Hared', 'password': 'password',
+                       'email': 'mhared@uottawa.ca', 'phone_number': '6137410001', 'tutor_description': sample_short_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 45, 'num_tutor_rating': 10, 'education': education[1]})
 
-encrypted_password = User.new(password: 'password').encrypted_password
-User.bulk_insert(:first_name, :last_name, :phone_number, :encrypted_password, :email, :uid,
-                 :agg_tutor_rating, :agg_user_rating, :num_tutor_rating, :num_user_rating, :tutor_description, :tutor_short_description) do |worker|
-  1000.times do |i|
-    worker.add(first_name: Faker::Name.first_name,
-               last_name: Faker::Name.last_name,
-               agg_tutor_rating: Faker::Number.between(10, 50),
-               num_tutor_rating: 10,
-               agg_user_rating: Faker::Number.between(10, 50),
-               num_user_rating: 10,
-               phone_number: 100000 + i,
-               encrypted_password: encrypted_password,
-               uid: "testemail#{i}@test.com",
-               email: "testemail#{i}@test.com",
-               tutor_description: sample_bio,
-               tutor_short_description: sample_caption,
-               education: education[i%3])
-  end
-end
+tutor_peng = User.create!({'first_name': 'Peng', 'last_name': 'Liu', 'password': 'password',
+                       'email': 'pliu@uottawa.ca', 'phone_number': '6137410002', 'tutor_description': sample_short_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 30, 'num_tutor_rating': 10, 'education':  education[2]})
 
-Course.bulk_insert do |worker|
-  worker.add(course_prefix: 'CSI', course_code: '2132', course_name: 'Database I')
-  worker.add(course_prefix: 'CSI', course_code: '3131', course_name: 'Operating Systems')
-  worker.add(course_prefix: 'CSI', course_code: '4107', course_name: 'Information Retrieval')
-  worker.add(course_prefix: 'SEG', course_code: '4911', course_name: 'Capstone (Part 2)')
-  worker.add(course_prefix: 'CEG', course_code: '3185', course_name: 'Data communications')
-end
+tutor_leila = User.create!({'first_name': 'Leila', 'last_name': 'Compaore', 'password': 'password',
+                       'email': 'lcompaore@uottawa.ca', 'phone_number': '6137410003', 'tutor_description': sample_short_bio, 'tutor_short_description': sample_caption,
+                       'agg_tutor_rating': 16, 'num_tutor_rating': 10, 'education': education[2]})
 
-user_ids = User.pluck(:id)
-course_ids = Course.pluck(:id)
+course_4145 = Course.create!({'course_prefix': 'SEG', 'course_code': '4145', 'course_name': 'Real Time Systems'})
+course_4911 = Course.create!({'course_prefix': 'SEG', 'course_code': '4911', 'course_name': 'Capstone Part 2'})
+course_2141 = Course.create!({'course_prefix': 'CVG', 'course_code': '2141', 'course_name': 'Civil Engineering Materials'})
+course_1370 = Course.create!({'course_prefix': 'ADM', 'course_code': '1370', 'course_name': 'Applications of IT for Business'})
+course_3105 = Course.create!({'course_prefix': 'CSI', 'course_code': '3105', 'course_name': 'Design and Analysis of Algorithms'})
+course_2372 = Course.create!({'course_prefix': 'CSI', 'course_code': '2372', 'course_name': 'Programming Concepts With C++'})
 
-TutorSubject.bulk_insert(:course_id, :user_id, :rate, ignore: true) do |worker|
-  1000.times do |i|
-    worker.add(course_id: course_ids.sample,
-               user_id: user_ids.sample,
-               rate: rand(100) + 1)
-  end
-end
+subject1 = TutorSubject.create!({'user_id': tutor_salman.id, 'course_id': course_2372.id, 'rate': 20})
+subject2 = TutorSubject.create!({'user_id': tutor_salman.id, 'course_id': course_4911.id, 'rate': 15})
+subject3 = TutorSubject.create!({'user_id': tutor_sarmad.id, 'course_id': course_4145.id, 'rate': 25})
+subject4 = TutorSubject.create!({'user_id': tutor_sarmad.id, 'course_id': course_3105.id, 'rate': 15})
+subject5 = TutorSubject.create!({'user_id': tutor_muraad.id, 'course_id': course_1370.id, 'rate': 15})
+subject6 = TutorSubject.create!({'user_id': tutor_muraad.id, 'course_id': course_3105.id, 'rate': 35})
+subject7 = TutorSubject.create!({'user_id': tutor_peng.id, 'course_id': course_2372.id, 'rate': 20})
+subject8 = TutorSubject.create!({'user_id': tutor_peng.id, 'course_id': course_4911.id, 'rate': 15})
+subject9 = TutorSubject.create!({'user_id': tutor_leila.id, 'course_id': course_2141.id, 'rate': 25})
+subject10 = TutorSubject.create!({'user_id': tutor_leila.id, 'course_id': course_3105.id, 'rate': 20})
 
-tutor_subject_ids = TutorSubject.pluck(:id)
-tutor_ids = TutorSubject.distinct.pluck(:user_id)
 
-PendingTutorRequest.bulk_insert(:tutor_subject_id, :student_id, :tutor_id, ignore: true) do |worker|
-  200.times do
-    worker.add(tutor_subject_id: tutor_subject_ids.sample,
-               student_id: user_ids.sample,
-               tutor_id: tutor_ids.sample)
-  end
-end
+tutee = User.create!({'first_name': 'Test', 'last_name': 'Student', 'password': 'password',
+                       'email': 'student@test.com', 'phone_number': '6138459999', 'tutor_hidden': 0})
 
-AcceptedTutorRequest.bulk_insert(:tutor_subject_id, :student_id, :tutor_id, ignore: true) do |worker|
-  200.times do
-    worker.add(tutor_subject_id: tutor_subject_ids.sample,
-               student_id: user_ids.sample,
-               tutor_id: tutor_ids.sample)
-  end
-end
-
-# for demo
-Course.create!({'course_prefix': 'SEG', 'course_code': '4910', 'course_name': 'Capstone (Part 1)'})
